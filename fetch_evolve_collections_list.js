06d4eaf6ede25f7af9f245070679b1b232057273
Chris Gianelle <cgianelle@cipherhealth.com>
2021-10-04 17:04:58 -0400

    more refactoring

--- fetch_evolve_collections_list.js
+++ fetch_evolve_collections_list.js
@@ -1,6 +1,8 @@
 const {MongoDBConnect} = require('./lib/mongo_utils');
 var fs = require('fs');
 
+const FILE_PATH = './reports/collection_list.txt';
+
 async function fetchCollectionList() {
     const mongo_connection = new MongoDBConnect();
     await mongo_connection.connect();
@@ -12,13 +14,20 @@ async function fetchCollectionList() {
 }
 
 fetchCollectionList().then(collections => {
-    console.log("Writing collections out to ./collection_list.txt");
-    collections.forEach(collection => {
-        fs.appendFile('./collection_list.txt', collection.collectionName+'\n', err => {
-            if (err) {
-                console.error(err);
-            }
-        });
+    const collectionNames = collections.map(collection => {
+        return collection.collectionName;
+    });
+
+    collectionNames.sort();
+
+    console.log(collectionNames);
+
+    fs.writeFile(FILE_PATH, JSON.stringify(collectionNames), err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`collections report can be found in ${FILE_PATH}`);
+        }
     });
 }).catch(err => {
     console.error(err);
