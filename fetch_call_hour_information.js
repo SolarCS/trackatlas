6ac211a0dc20848f6afb4d272a3edf558a76f589
Chris Gianelle <cgianelle@cipherhealth.com>
2022-06-20 20:01:27 +0000

    Fixing results

--- fetch_call_hour_information.js
+++ fetch_call_hour_information.js
@@ -6,6 +6,8 @@ const {
 } = require('./lib/mongo_utils');
 const {ObjectId} = require('mongodb');
 
+var results = [];
+
 //Dates must be entered in YYYY-MM-DD
 var [,, start_date, end_date, output_file] = process.argv;
 
@@ -61,30 +63,32 @@ async function main(start_date, end_date, output_file) {
     
     async function fetch_outreach_program_information(call_hour_information) {
         console.log('Fetch Outreach Program Information');
-        // console.log(`call_hour_information: ${call_hour_information}`);
+        //console.log(call_hour_information);
 
         const outreach_program = new OutreachProgram(mongo_connection);
         const care_provider = new CareProviders(mongo_connection);
-        const results = [];
+        //const results = [];
 
-        for (const call_hour in call_hour_information) {
-            const program = await outreach_program.getById(call_hour.outreach_program_id, {
+        for (const call_hour of call_hour_information) {
+            console.log(call_hour);
+	    const program = await outreach_program.getById(call_hour.outreach_program_id, {
                 _id: 0,
                 care_provider_id: 1,
                 outreach_use_case: 1
             });
 
-            const cp = await care_provider.getById(program.care_provider_id, {
+            console.log(program[0]);
+	    const cp = await care_provider.getById(program[0].care_provider_id, {
                 _id: 0,
                 time_zone: 1,
                 name: 1
             });
-
-            results.concat({
-                call_hour,
-                outreach_use_case: program.outreach_use_case,
-                time_zone: cp.time_zone,
-                care_provider_name: cp.name
+            console.log(cp[0]);
+            results.push({
+                ...call_hour,
+                outreach_use_case: program[0].outreach_use_case,
+                time_zone: cp[0].time_zone,
+                care_provider_name: cp[0].name
             });
         }
 
