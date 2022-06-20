a1d7656ea5cb92111447daaac6bb9b4b51bf603f
Chris Gianelle <cgianelle@cipherhealth.com>
2022-06-20 15:36:08 -0400

    Fetch the program and CP information

--- fetch_call_hour_information.js
+++ fetch_call_hour_information.js
@@ -1,7 +1,8 @@
 const {
     MongoDBConnect, 
     CallHourInfo,
-    CareProviders
+    CareProviders,
+    OutreachProgram
 } = require('./lib/mongo_utils');
 const {ObjectId} = require('mongodb');
 
@@ -61,7 +62,33 @@ async function main(start_date, end_date, output_file) {
     async function fetch_outreach_program_information(call_hour_information) {
         console.log('Fetch Outreach Program Information');
         // console.log(`call_hour_information: ${call_hour_information}`);
-        return call_hour_information;
+
+        const outreach_program = new OutreachProgram(mongo_connection);
+        const care_provider = new CareProviders(mongo_connection);
+        const results = [];
+
+        for (const call_hour in call_hour_information) {
+            const program = await outreach_program.getById(call_hour.outreach_program_id, {
+                _id: 0,
+                care_provider_id: 1,
+                outreach_use_case: 1
+            });
+
+            const cp = care_provider.getById(program.care_provider_id, {
+                _id: 0,
+                time_zone: 1,
+                name: 1
+            });
+
+            results.concat({
+                call_hour,
+                outreach_use_case: program.outreach_use_case,
+                time_zone: cp.time_zone,
+                care_provider_name: cp.name
+            });
+        }
+
+        return results;
     }
     
     async function fetch_care_provider_information(call_hour_information) {
